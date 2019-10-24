f49a12c4203af958f6c23a49d0f8abb5c91fc771
Jose Garfias Lopez <garfiaslopez@hotmail.com>
2019-10-24 03:26:50 -0500

    some corrections

--- Practica/main.cpp
+++ Practica/main.cpp
@@ -9,8 +9,9 @@
 
 using namespace std; 
 
-#define POPULATION_SIZE 50
-#define SIZE_OF_CHROMOSOME 13
+#define POPULATION_SIZE 100
+#define SIZE_OF_CHROMOSOME 390
+#define SIZE_OF_CHUNK 13
 const string GENES = "01"; 
 
 int random_num(int start, int end) {
@@ -27,7 +28,7 @@ float random_float_num(float start, float end) {
   
 char mutated_genes() { 
     int r = random_num(0, GENES.size() - 1); 
-    return GENES[r]; 
+    return GENES[r];
 } 
   
 string create_gnome() {
@@ -45,12 +46,13 @@ public:
     float fitness; 
     Individual(string chromosome); 
     vector<Individual> mate(Individual parent2); 
-    float cal_fitness(); 
+    void calc_fitness(); 
+    void uniform_mutate(); 
 };
   
 Individual::Individual(string chromosome) {
     this->chromosome = chromosome; 
-    fitness = cal_fitness();  
+    this->calc_fitness();  
 }; 
 
 vector<Individual> Individual::mate(Individual partner) {
@@ -62,11 +64,11 @@ vector<Individual> Individual::mate(Individual partner) {
     int point_of_cross = random_num(1, SIZE_OF_CHROMOSOME - 1);
 
     for (int i=0; i <= point_of_cross; i++) {
-        parentA_A += chromosome[i];
+        parentA_A += this->chromosome[i];
         parentB_A += partner.chromosome[i];
     }
     for (int i=point_of_cross+1; i < SIZE_OF_CHROMOSOME; i++) {
-        parentA_B += chromosome[i];
+        parentA_B += this->chromosome[i];
         parentB_B += partner.chromosome[i];
     }
 
@@ -91,21 +93,46 @@ int binary_to_decimal(string n) {
 } 
 
 float chromosome_to_x(string chromosome) {
-    float aj = -5;
-    float bj = 5;
+    float aj = -30;
+    float bj = 30;
     return aj + (float)binary_to_decimal(chromosome) * ((bj - aj) / ((pow(2,13) - 1)));
 }
 
-float fitness_func(float x) {
-    return 0.65 - (0.75/(1 + powf(x, 2))) - (0.65 * x * atanf(1/x));
+float fitness_func(string chromosome) {
+    // CALCULE X PER SUBCHROMOSOME IN CHROMOSOME
+    vector<float> x_values;
+    int chunk = 0;
+    string sub_chromosome = "";
+    for (int i=0; i<SIZE_OF_CHROMOSOME; i++) {
+        sub_chromosome += chromosome[i];
+        if (sub_chromosome.length() == 13) {
+            x_values.push_back(chromosome_to_x(sub_chromosome));
+            sub_chromosome = "";
+        }
+    }
+
+    // EVAL FITNESS FUNCTION
+    float sum_result = 0;
+    for (int i=0; i<x_values.size(); i++) {
+        float Xi = x_values[i];
+        sum_result += abs(100 * powf((Xi+1)-(powf(Xi, 2)), 2) + powf((Xi-1), 2));
+    }
+    return sum_result;
 }
 
-float Individual::cal_fitness() {
-    return fitness_func(chromosome_to_x(chromosome));
+void Individual::calc_fitness() {
+    this->fitness = fitness_func(this->chromosome);
+};
+
+void Individual::uniform_mutate() {
+    int random_position = random_num(0, SIZE_OF_CHROMOSOME - 1);
+    char random_gene = mutated_genes();
+    this->chromosome[random_position] = random_gene;
+    this->calc_fitness();
 };
   
 bool operator<(const Individual &ind1, const Individual &ind2) {
-    return ind1.fitness < ind2.fitness; 
+    return ind1.fitness > ind2.fitness; 
 }
 
 Individual roulete_selection (vector<Individual> population) {
@@ -134,33 +161,53 @@ Individual roulete_selection (vector<Individual> population) {
             break;
         }
     }
-    
+
     return Individual(population[individual_i].chromosome); 
+}
+
+void print_generation(int genration_number, vector<Individual> population, bool best_member) {
+    if (best_member) {
+        cout<< "Generation: " << genration_number << "\t"; 
+        cout<< "String: "<< population[0].chromosome <<"\t"; 
+        cout<< "Value: "<< chromosome_to_x(population[0].chromosome) <<"\t"; 
+        cout<< "Fitness: "<< population[0].fitness << "\n";
+    } else {
+        for (int i=0; i<population.size(); i++) {
+            cout<< "Generation: " << genration_number << "\t"; 
+            cout<< "String: "<< population[i].chromosome <<"\t"; 
+            cout<< "Value: "<< chromosome_to_x(population[i].chromosome) <<"\t"; 
+            cout<< "Fitness: "<< population[i].fitness << "\n";
+        }
+    }
+    
 }
   
 int main() {
     srand((unsigned)(time(0)));
     int generation = 0; 
     vector<Individual> population; 
+    vector<Individual> new_generation;
 
     // create initial population
     for(int i=0; i<POPULATION_SIZE; i++) {
         string gnome = create_gnome();
         population.push_back(Individual(gnome));
-    } 
+    }
   
-    while(generation < 50) {
+    while(generation <= 3000) {
+        new_generation.clear(); // clear the array for new generation
+
+        // SORT THE GENERATION BY FITNESS
         sort(population.begin(), population.end()); 
-  
-        vector<Individual> new_generation; 
-  
+        print_generation(generation, population, true);
+
         // ELITISM
         int s = (20*POPULATION_SIZE)/100; 
         for(int i = 0; i<s; i++){
             new_generation.push_back(population[i]);
         }
     
-        // CROSSOVER
+        // SELECTION AND CROSSOVER
         s = ((80*POPULATION_SIZE)/100 ) / 2;
         for(int i = 0; i<s; i++) {
             Individual parent1 = roulete_selection(population);
@@ -170,19 +217,10 @@ int main() {
             new_generation.push_back(childs[1]);
         }
 
-        population = new_generation;
+        // MUTATION: 1/L
+        new_generation[0].uniform_mutate(); // the best
 
-        
-        cout<< "Generation: " << generation << "\t"; 
-        cout<< "String: "<< population[0].chromosome <<"\t"; 
-        cout<< "Value: "<< chromosome_to_x(population[0].chromosome) <<"\t"; 
-        cout<< "Fitness: "<< population[0].fitness << "\n"; 
-  
-        generation++; 
-     } 
-    
-    cout<< "Generation: " << generation << "\t"; 
-    cout<< "String: "<< population[0].chromosome <<"\t"; 
-    cout<< "Value: "<< chromosome_to_x(population[0].chromosome) <<"\t"; 
-    cout<< "Fitness: "<< population[0].fitness << "\n"; 
+        population = new_generation;
+        generation++;
+     }
 }
